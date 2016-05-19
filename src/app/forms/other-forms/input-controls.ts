a8496d31e8739ebfc55c8a3bb7a57bf11d3a7d49
Nick Lowman <digital@nicklowman.co.uk>
2016-05-19 11:23:49 +0100

    Updated form control

--- src/app/forms/other-forms/input-controls.ts
+++ src/app/forms/other-forms/input-controls.ts
@@ -8,16 +8,18 @@ import {
 @Component({
     selector: 'input-controls',
     template: require('./input-controls.html'),
-    directives:[FORM_DIRECTIVES]
+    directives: [FORM_DIRECTIVES]
 })
 
 export class InputControls implements CanReuse {
 
     gender:string;
-    javascript = false;
-    angular = false;
-    csharp = false;
-    name = 'Two way bound';
+    javascript:boolean = false;
+    angular:boolean = false;
+    csharp:boolean = false;
+    name:string = 'Two way bound';
 
-    routerCanReuse(next: ComponentInstruction, prev: ComponentInstruction) { return false; }
+    routerCanReuse(next:ComponentInstruction, prev:ComponentInstruction) {
+        return false;
+    }
 }

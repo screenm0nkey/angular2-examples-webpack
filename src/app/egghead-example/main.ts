5709e4ff02e62cac46e85802d4a4dbbc7f97fc7e
Nick Lowman <digital@nicklowman.co.uk>
2016-11-24 16:27:47 +0000

    Updates

--- src/app/egghead-example/main.ts
+++ src/app/egghead-example/main.ts
@@ -4,7 +4,21 @@ import {TodoService} from "./services/todo-service";
 @Component({
     selector: 'eggehead-app',
     providers : [TodoService], // this provides the service to all of it's children components
-    template: require('./main.html'),
+    template: `
+      <div class="egghead">
+        <h4>Egghead example from john Liquist</h4>
+<pre>Take a look at the Todo service and see how the it keeps the data immutable
+Also see how the pipes work by creating a new data array rather than mutating the array</pre>
+            <p><a target="_blank" href="https://egghead.io/series/angular-2-fundamentals">Fundamentals videos</a> </p>
+        
+            <search-box (update)="term = $event"></search-box>
+            <new-todo></new-todo>
+            <status-selector (select)="status = $event"></status-selector>
+            <todo-list
+                [status]="status"
+                [term]="term"></todo-list>
+      </div>
+    `,
     styles : [`
         .egghead {    
             background: lightpink;

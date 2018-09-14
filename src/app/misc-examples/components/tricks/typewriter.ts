00cdcd8088183c49fc0dd8fb401a4e2fc52a1491
Nicholas Lowman <digital@nicklowman.co.uk>
2018-09-14 09:44:25 +0100

    Updating components to RXJS6

--- src/app/misc-examples/components/tricks/typewriter.ts
+++ src/app/misc-examples/components/tricks/typewriter.ts
@@ -1,10 +1,6 @@
-import {Component} from "@angular/core";
-import {from, interval} from "rxjs";
+import {Component, OnInit} from "@angular/core";
+import {from, interval, Observable} from "rxjs";
 import {scan, zip} from 'rxjs/operators';
-import "rxjs/add/observable/from";
-import "rxjs/add/observable/interval";
-import "rxjs/add/operator/scan";
-import "rxjs/add/operator/zip";
 
 @Component({
   selector: "typewriter",
@@ -13,13 +9,18 @@ import "rxjs/add/operator/zip";
       {{typewriter$ | async}}
     </div>`
 })
-export class Typewriter {
-  text = `Bacon ipsum dolor amet tongue short loin short ribs pork chop drumstick picanha 
+export class Typewriter implements OnInit {
+  text: string;
+  typewriter$: Observable<string>;
+
+  ngOnInit() {
+    this.text = `Bacon ipsum dolor amet tongue short loin short ribs pork chop drumstick picanha 
             pancetta shank cow shoulder salami corned beef swine pig sausage. Rump flank 
             sirloin kielbasa jowl chuck sausage jerky short loin chicken. Flank sirloin frankfurter 
             corned beef turducken. Beef ribs salami ribeye, t-bone meatloaf flank jerky tongue turkey`;
 
-  typewriter$ = from(this.text.split(""))
-    .pipe(scan((acc, curr) => acc + curr))
-    .pipe(zip(interval(50), x => x));
+    this.typewriter$ = from(this.text.split(""))
+      .pipe(scan((acc, curr) => acc + curr))
+      .pipe(zip(interval(50), x => x));
+  }
 }
